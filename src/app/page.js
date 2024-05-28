d05a1391d39d366f93b0e78588df522a633b73b4
Compulink-Dev <devcompulink@gmail.com>
2024-05-28 16:46:19 +0200

    responsiveness check on clients needs

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <Hero />
       <Services />
       <About />
-      <ClientNeeds />
+      {/* <ClientNeeds /> */}
       <Partners />
       <Promotion />
       <Testimonials />
