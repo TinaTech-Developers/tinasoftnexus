59c23587190968daeaea068d24ad8290b913bf5d
TinaTech-Consulting <>
2024-11-16 10:56:18 +0200

    edited button length

--- src/app/(main)/components/Services.jsx
+++ src/app/(main)/components/Services.jsx
@@ -75,7 +75,7 @@ function Service({ image, heading, desc, link }) {
       </div>
       <h1 className="text-xl font-semibold py-4 ">{heading}</h1>
       <p className="">{desc}</p>
-      <div className="w-20 md:w-[93px] mt-4 z-10">
+      <div className="w-20 md:w-[98px] mt-4 z-10">
         <Button name={"More Info"} link={""} />
       </div>
     </motion.div>
